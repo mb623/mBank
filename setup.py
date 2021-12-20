5a9784a37234b9ac3e6455531638ccfd158fba72
mariusz <52469667+mariuszbujwid@users.noreply.github.com>
2021-12-20 23:52:48 +0100

    # dodanie setup.py
    Signed-off-by: mariusz <52469667+mariuszbujwid@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mBank',
+    version='0.1',
+    author='Mariusz',
+    author_email='bujwidmariusz@gmail.com',
+    description='Zadanie_testowe',
+    chrome_version='96.0.4664.110',
+    chomewebdriver_version='96.0.4664.45',
+)
